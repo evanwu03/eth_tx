ae7bd1424198ae2979cbf31586aa3900a3636be4
Evan Wu <ewu43951@gmail.com>
2026-06-11 13:04:02 -0500

    Set up cocotb + verilator basic testbench

---
+++ rtl/eth_tx_top.sv
@@ -0,0 +1,10 @@
+
+
+module eth_tx_top (
+    input logic i_clk,
+    input logic i_rst_n
+);
+
+
+
+endmodule : eth_tx_top
---
+++ tb/eth_tx.py
@@ -0,0 +1,49 @@
+
+import os
+from pathlib import Path
+import cocotb
+from cocotb.triggers import RisingEdge, Timer
+from cocotb_tools.runner import get_runner
+
+
+@cocotb.test()
+async def test_hello(dut):
+    assert(True)
+
+
+
+def test_eth_tx_runner():
+    
+    sim = os.getenv("SIM", "verilator")
+    proj_path = Path(__file__).resolve().parent.parent
+
+    sources = [ 
+        proj_path / "rtl" / "eth_tx_top.sv",
+        ]
+
+    runner = get_runner(sim)
+
+    parameters = {
+
+    }
+
+    runner.build(
+        sources=sources,
+        hdl_toplevel="eth_tx_top",
+        parameters=parameters,
+        build_dir="sim_build/eth_tx",
+        always=True,
+        clean=True
+        
+    )
+
+    runner.test(
+        hdl_toplevel="eth_tx_top",
+        test_module="eth_tx",
+        parameters=parameters,
+        build_dir="sim_build/eth_tx",
+        
+    )
+
+if __name__ == "__main__":
+    test_eth_tx_runner()
